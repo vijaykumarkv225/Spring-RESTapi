5a28ec160ac97c4419e79511d51dbcd7ce011d6f
vgaju <vgajula002@gmail.com>
2023-11-27 15:50:04 +0530

    new project added

---
+++ src/main/java/Microservices/com/SpringRESTapi/SpringResTapiApplication.java
@@ -0,0 +1,13 @@
+package Microservices.com.SpringRESTapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResTapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResTapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Microservices/com/SpringRESTapi/SpringResTapiApplicationTests.java
@@ -0,0 +1,13 @@
+package Microservices.com.SpringRESTapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResTapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
